002b08e3440688d8d9a8b005e6ed17b71a226a8d
Sebastian Calderon <jhonnseca0599@gmail.com>
2021-02-10 11:20:34 -0500

    Routers y Sass configurados

--- src/JournalApp.js
+++ src/JournalApp.js
@@ -1,11 +1,7 @@
-import React from 'react'
-
+import React from "react";
+import AppRouter from "./Components/Routers/AppRouter";
 const JournalApp = () => {
-    return (
-        <div>
-          <h1>Journal App</h1>  
-        </div>
-    )
-}
+  return <AppRouter />;
+};
 
-export default JournalApp
+export default JournalApp;
